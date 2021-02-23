9ebab17d0538063f28cc0b59d70ca5ccaf93a2ab
TheoWhite <48633634+TheoWhite@users.noreply.github.com>
2021-02-23 15:50:29 -0600

    Added the discordRequestHandler.

---
+++ discordRequestHandler.py
@@ -0,0 +1,9 @@
+import discord
+
+async def handle(message):
+    if message.content.startswith('!Cancel'):
+         #Cancel function
+         pass
+    if message.content.startswith("!Status"):
+        #Status function
+        await message.channel.send("The status of this order is...")
