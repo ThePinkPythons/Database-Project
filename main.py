dbd54d21db125c35c633c9aaf6eff6763042bdfa
Andrew Evans <asevans48@gmail.com>
2021-02-28 03:21:38 -0700

    sql lite unitteests

--- main.py
+++ main.py
@@ -2,20 +2,29 @@
 CS3250 Project. Includes Discord Chat Bot and CSV Reader
 
 Usage:
+    main.py <csv_path>
+    main.py <csv_path> [--db=<db>]
     main.py <csv_path> [--table=<table>]
     main.py <csv_path> [--headers=<headers>]
     main.py <csv_path> [--table=<table>] [--headers=<headers>]
     main.py <csv_path> [--headers=<headers>] [--table=<table>]
+    main.py <csv_path> [--db=<db>] [--headers=<headers>] [--table=<table>]
+    main.py <csv_path> [--headers=<headers>] [--db=<db>] [--table=<table>]
+    main.py <csv_path> [--headers=<headers>] [--table=<table>] [--db=<db>]
+    main.py <csv_path> [--table=<table>] [--headers=<headers>] [--db=<db>]
+    main.py <csv_path> [--table=<table>] [--db=<db>] [--headers=<headers>]
 
 Options:
   -h --help                Show this screen.
   --version                Show version.
   --table=<table>          Name of the table in sqlite3
   --headers=<headers>      Headers for the csv file in the order they appear
+  --db=<db>                Database to use
 """
 
 from docopt import docopt
 
 
 if __name__ == "__main__":
-    pass
+    arguments = docopt(__doc__, version='Database Project 0.1')
+    database = arguments.get()
