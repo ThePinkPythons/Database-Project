c40c5d6a5314a179a3b767b3224dc268e877396d
Andrew Evans <asevans48@gmail.com>
2021-02-28 23:19:31 -0700

    crud in test

--- db/io/manager.py
+++ db/io/manager.py
@@ -2,11 +2,11 @@
 Entry point utilities for reading db and storing in sql
 """
 
-import sqlite3
-
 from db.crud.executor import create_records
+from db.io.sink import CSVSink
 from db.io.source import CSVSource
 from db.sql.connection.singleton import Database
+from db.sql.query.builder import Select
 
 
 def write_csv_to_sql(filepath, db=":memory:", table="products", headers=None, table_mappings=None, batch_size=100):
@@ -20,18 +20,34 @@ def write_csv_to_sql(filepath, db=":memory:", table="products", headers=None, ta
     :param table_mappings:  Table mappings for creation or None to avoid table creation
     :param batch_size:  Number of records per batch
     """
-    db = Database.instance(db, table, table_mappings)
+    conn = Database.instance(db, table, table_mappings)
     csv = CSVSource(filepath, headers=headers, batch_size=batch_size)
     for batch in csv:
-        create_records(db, batch)
+        create_records(conn, batch)
 
 
-def read_csv_from_sql(db, sql, fpath, table="products"):
+def write_csv_from_sql(db, headers, fpath, table="products", batch_size=10000):
     """
     Writes the db file stored in the database to the file
 
     :param db: The db to write
+    :param headers: The headers to use in the select
     :param fpath:   The file path
     :param table:   The table to insert into
     """
-    pass
+    conn = Database.instance(db)
+    c = conn.cursor()
+    csv = CSVSink(fpath, headers)
+    select = Select(table, headers)
+    select = str(select)
+    batch = []
+    for record in c.execute(str(select)):
+        rdict = dict(zip(headers, record))
+        batch.append(rdict)
+        if len(batch) > batch_size:
+            csv.write_rows(batch)
+            batch = []
+    if len(batch) > 0:
+        csv.write_rows(batch)
+    csv.close()
+    del batch
