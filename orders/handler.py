b3532dfd72a7d259f792ee82c3871e3b6fc67084
Andrew Evans <asevans48@gmail.com>
2021-03-02 14:31:22 -0700

    order in place

--- orders/handler.py
+++ orders/handler.py
@@ -1,11 +1,42 @@
 """
-Order operations
+Order class
 """
 
 
-def create_order_table():
+class Order(object):
     """
-    Create the ore
-    :return:
+    Order Object
     """
-    pass
+
+    def __init__(self, email, address, city, state, zip):
+        """
+        Constructor
+
+        :param email:   Email address
+        :param address:   Shipping address
+        :param city:    Shipping city
+        :param state:   Shipping state
+        :param zip:     Shipping zip code
+        """
+        self.email = email
+
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+
+        self.products = []
+
+    def add_product(self, product_id, qty):
+        """
+        Add a product to the order
+
+        :param product_id:  Product id
+        :param qty:     Quantity of the
+        :return:
+        """
+        product = (product_id, qty)
+        self.products.append(product)
+
+    def del_product(self, product_id):
+        self.products = [x for x in self.products if x[0] != product_id]
