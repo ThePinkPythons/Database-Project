2c3a6e1c6681e6d307d19d34dba62a548235ad01
Erica White <erica.white09@yahoo.com>
2021-03-01 18:30:11 -0700

    updated order class and sql

--- discord/discordRequestHandler.py
+++ discord/discordRequestHandler.py
@@ -6,3 +6,8 @@ async def handle(message):
     if message.content.startswith("!Status"):
         #Status function
         await message.channel.send("The status of this order is...")
+
+    if message.content.startswith("!New"):
+        #New Order Function
+
+        await message.channel.send("The status of this order is...")
