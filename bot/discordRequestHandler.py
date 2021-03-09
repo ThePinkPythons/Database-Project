8062f7223f6c3575486a3ad85ad5e61554ca8852
TheoWhite <48633634+TheoWhite@users.noreply.github.com>
2021-03-09 16:41:53 -0600

    Updated the below function.
    This function should now be able to handle any integer not just 20 and if the function does not contain a number it should print out a simple guide on how to use the function.

--- bot/discordRequestHandler.py
+++ bot/discordRequestHandler.py
@@ -23,19 +23,33 @@ async def handle(message):
             '\n For products that cost less than $20 type: below20')
 
     if message.content.startswith('!ORDERS'):
-        await message.channel('Your past orders were: ')
+        pass
 
 
     if message.content.startswith('!RECOMMENDED'):
         await message.channel.send('This functionality currently does not exist. '
                                    'Please check during a later sprint. ')
 
-    if message.content.startswith('!BELOW20'):
-        _db = Database.instance(None)
-        sel = Select("products", ["product_id"])
-        sel.less_than_or_equal_to("sale_price", 20.0)
-        products = []
-        for product in get_record(sel):
-            products.append(product[0])
-        msg = "The products below $20 are: {}".format(str(products[:10]))
-        await message.channel.send(msg)
+    if message.content.startswith('!BELOW'):
+        """This function should now be able to handle more than just $20.
+            This function searches the product dB and displays the first
+            ten values under the price inputtued by the user.
+        """
+        #Substring the user's input to get the integer value.
+        below = message.content[6:]
+        #Check if the Substring contains a digit.
+        if(below.isdigit()):
+            _db = Database.instance(None)
+            sel = Select("products", ["product_id"])
+            sel.less_than_or_equal_to("sale_price", below)
+            products = []
+            for product in get_record(sel):
+                products.append(product[0])
+            msg = "The products below ", below , " are: {}".format(str(products[:10]))
+            await message.channel.send(msg)
+        else:
+            #Simple help response if the user inputs something other than a digit.
+            await message.channel.send(
+            'Please type a integer after !BELOW'
+            '\n Example !BELOW20'
+            '\n this will return the first ten items below $20')
