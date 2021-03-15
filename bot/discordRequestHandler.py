906be0ca36aeb289e2793abaa2dcf59bbfb7cd97
Roguma <roguma@msudenver.edu>
2021-03-15 11:17:20 -0600

    Merge branch 'Confirm-Orders-and-Customer-Orders-Clean-up' of C:\Users\knigh\OneDrive\Documents\GitHub\Database-Project with conflicts.

--- bot/discordRequestHandler.py
+++ bot/discordRequestHandler.py
@@ -18,38 +18,47 @@ async def handle(message):
 
     if message.content.startswith('!HELP'):
         await message.channel.send(
-            'For past orders type: orders'
-            '\n For recommended products type: recommended'
-            '\n For products that cost less than $20 type: below20')
+            'To create an account type: account'
+            '\nFor past orders type: orders'
+            '\nFor recommended products type: recommended'
+            '\nFor products that cost less than $20 type: below20'
+            '\nTo create a new order type: new')
 
     if message.content.startswith('!ORDERS'):
-        pass
-
+        await message.chnnel.send('This functionality currently does not exist. '
+                                  '\nPlease check during a later sprint. ')
 
     if message.content.startswith('!RECOMMENDED'):
         await message.channel.send('This functionality currently does not exist. '
-                                   'Please check during a later sprint. ')
+                                   '\nPlease check during a later sprint. ')
 
     if message.content.startswith('!BELOW'):
         """This function should now be able to handle more than just $20.
             This function searches the product dB and displays the first
-            ten values under the price inputtued by the user.
+            ten values under the price input by the user.
         """
-        #Substring the user's input to get the integer value.
+        # Substring the user's input to get the integer value.
         below = message.content[6:]
-        #Check if the Substring contains a digit.
-        if(below.isdigit()):
+        # Check if the Substring contains a digit.
+        if below.isdigit():
             _db = Database.instance(None)
             sel = Select("products", ["product_id"])
             sel.less_than_or_equal_to("sale_price", below)
             products = []
             for product in get_record(sel):
                 products.append(product[0])
-            msg = "The products below ", below , " are: {}".format(str(products[:10]))
+            msg = "The products below ", below, " are: {}".format(str(products[:10]))
             await message.channel.send(msg)
         else:
-            #Simple help response if the user inputs something other than a digit.
+            # Simple help response if the user inputs something other than a digit.
             await message.channel.send(
-            'Please type a integer after !BELOW'
-            '\n Example !BELOW20'
-            '\n this will return the first ten items below $20')
+                'Please type a integer after !BELOW'
+                '\n Example !BELOW20'
+                '\n this will return the first ten items below $20')
+
+    if message.content.startswith('!ACCOUNT'):
+        pass
+
+    if message.content.startswith('!NEW'):
+        pass
+
