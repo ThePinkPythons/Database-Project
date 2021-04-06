7ad56c84a182e15410fd305df8ffccdf3f23d5d3
Andrew Evans <asevans48@gmail.com>
2021-04-06 14:43:39 -0600

    added test for cancellation

--- orders/tests/test.py
+++ orders/tests/test.py
@@ -4,8 +4,24 @@ Tests for the table
 
 import unittest
 
+from db.sql.connection.singleton import Database
+from orders.handler import CancelOrder
+
 
 class OrderTests(unittest.TestCase):
+
+    def test_update_record(self):
+        mp = {
+            "order_id": "varchar",
+            "status": "varchar"
+        }
+        keys = mp.keys()
+        _db = Database.instance(":memory:", "test_table", mp)
+        data = [{
+            "order_id": "test_id",
+            "status": "active"
+        }]
+        CancelOrder(["order_id", "status"], "")
 
     def test_create_order_table(self):
         pass
