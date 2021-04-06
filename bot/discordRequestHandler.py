cf24bf8f3a564328c7002cb351aea2b1b47992b2
Rory <roguma@msudenver.edu>
2021-04-06 10:29:56 -0600

    Update discordRequestHandler.py
    added message to the function calls.
    
    (oops)

--- bot/discordRequestHandler.py
+++ bot/discordRequestHandler.py
@@ -17,7 +17,7 @@ async def handle(message):
     """Handler user message"""
     if message.content.startswith('!ADD'):
         """This function creates a new user. """
-        await user_functions.add_user()
+        await user_functions.add_user(message)
 
     if message.content.startswith('!CANCEL'):
         """This function will set the status of an order to cancelled."""
@@ -29,26 +29,26 @@ async def handle(message):
 
     if message.content.startswith('!NEW'):
         # New Order Function
-        await orderHandler.place_order()
+        await orderHandler.place_order(message)
 
     if message.content.startswith('!HELP'):
         """This will call the help function from 'user_functions' and display commands."""
-        await user_functions.get_help()
+        await user_functions.get_help(message)
 
     if message.content.startswith('!ORDERS'):
         """This function displays the users order history by order Id with product_id, 
            quantity, price and status. 
         """
-        await orderHandler.orders()
+        await orderHandler.orders(message)
 
     if message.content.startswith('!RECOMMENDED'):
         """This function currently returns a list of five random items by their product Id.
         """
-        await recommend.recommend()
+        await recommend.recommend(message)
 
     if message.content.startswith('!BELOW'):
         """This function should now be able to handle more than just $20.
            This function searches the product dB and displays the first
            ten values under the price input by the user.
         """
-        await recommend.below()
+        await recommend.below(message)
