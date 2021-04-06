581eb05e6d4b1720d8f342a3238358d89036f46b
Andrew Evans <asevans48@gmail.com>
2021-04-06 13:52:06 -0600

    updated to most recent version

--- discord/discordRequestHandler.py
+++
@@ -1,8 +0,0 @@
-
-async def handle(message):
-    if message.content.startswith('!Cancel'):
-         #Cancel function
-         pass
-    if message.content.startswith("!Status"):
-        #Status function
-        await message.channel.send("The status of this order is...")
