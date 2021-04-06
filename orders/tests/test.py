1d5fc1ee54007b145fa9a4550dbfcb30096fe4dc
Andrew Evans <asevans48@gmail.com>
2021-04-06 14:55:49 -0600

    cancel working

--- orders/tests/test.py
+++ orders/tests/test.py
@@ -6,7 +6,7 @@ import unittest
 from _msi import CreateRecord
 
 from db.sql.connection.singleton import Database
-from orders.handler import CancelOrder, Order, ORDER_TABLE_MAPPING
+from orders.handler import CancelOrder, Order, ORDER_TABLE_MAPPING, GetOrders
 
 
 class OrderTests(unittest.TestCase):
@@ -17,8 +17,16 @@ class OrderTests(unittest.TestCase):
         order.quantity(1)
         order.price(2)
         order.save()
+        orders = GetOrders("test_author")
+        records = orders.query()
+        assert len(records) == 1
         cancel = CancelOrder("test_id")
         cancel.cancel()
+        orders = GetOrders("test_author")
+        records = orders.query()
+        assert len(records) == 1
+        order = records[0]
+        assert order["status"] == "cancelled"
 
 
     def test_create_order_table(self):
