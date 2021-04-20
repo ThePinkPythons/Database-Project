99b9814f66d7c8b4d45b2c7bf72462a08a1a36ee
Theo <theowhite4@gmail.com>
2021-04-19 22:44:24 -0500

    Website
    Kept working on the website. Implemented the logo and some generic data.

--- website/website/views.py
+++ website/website/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+args = {"today":55,"yesterday":20,"last_item_sold":'LBH6UWAEK4IK',"most_items_sold":"XLS48IBKVC7C"}
+
 def data(request):
-    return render(request,'main.html')
+    return render(request,'main.html',args)
