6a4fb51b312e575a18b4618c8c55ba4080c6d228
Andrew Evans <asevans48@gmail.com>
2021-04-29 14:40:35 -0600

    removed unrelated note

--- website/website/views.py
+++ website/website/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-args = {"today":55,"yesterday":20,"last_item_sold":'LBH6UWAEK4IK',"most_items_sold":"XLS48IBKVC7C"}
+args = {"today": 55, "yesterday": 20, "last_item_sold": ' LBH6UWAEK4IK', "most_items_sold": "XLS48IBKVC7C"}
+
 
 def data(request):
-    return render(request,'main.html',args)
+    return render(request, 'main.html', args)
