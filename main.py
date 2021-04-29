2ef63912b8a57814e877e38615ec42cf5eff7c79
Andrew Evans <asevans48@gmail.com>
2021-04-29 15:38:01 -0600

    patched app to allow you to choose what to run and how

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 CS3250 Project. Includes Discord Chat Bot and CSV Reader.
 
 Usage:
-    main.py [--csv_path=<csv_path>]
-    main.py [--csv_path=<csv_path>] [--db=<db>] [--create=<create>]
-    main.py [--csv_path=<csv_path>] [--table=<table>]
-    main.py [--csv_path=<csv_path>] [--headers=<headers>]
-    main.py [--csv_path=<csv_path>] [--table=<table>] [--headers=<headers>]
-    main.py [--csv_path=<csv_path>] [--headers=<headers>] [--table=<table>]
+    main.py [--csv_path=<csv_path>] [--website_only|--discord_only]
+    main.py [--csv_path=<csv_path>] [--db=<db>] [--create=<create>] [--website_only|--discord_only]
+    main.py [--csv_path=<csv_path>] [--table=<table>] [--website_only|--discord_only]
+    main.py [--csv_path=<csv_path>] [--headers=<headers>] [--website_only|--discord_only]
+    main.py [--csv_path=<csv_path>] [--table=<table>] [--headers=<headers>] [--website_only|--discord_only]
+    main.py [--csv_path=<csv_path>] [--headers=<headers>] [--table=<table>] [--website_only|--discord_only]
     main.py [--csv_path=<csv_path>] [--db=<db>] [--headers=<headers>] [--table=<table>] [--create=<create>]
     main.py [--csv_path=<csv_path>] [--headers=<headers>] [--db=<db>] [--table=<table>] [--create=<create>]
     main.py [--csv_path=<csv_path>] [--headers=<headers>] [--table=<table>] [--db=<db>] [--create=<create>]
@@ -21,10 +21,14 @@ Options:
   --db=<db>                Database to use
   --create=<create>        Tells the program to create the database
   --csv_path=<csv_path>    Path to the product data csv for initializing the database
+  --website_only           Start only the analytics website
+  --discord_only           Only start the discord app
 """
 
 import json
+import logging
 import os
+import sys
 import threading
 
 from docopt import docopt
@@ -97,14 +101,45 @@ def start_website():
 
 
 if __name__ == "__main__":
+    # set logger
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+    # get arguments from docopt
     arguments = docopt(__doc__, version='Database Project 0.1')
+    logging.info("Database Project 0.1")
     threads = []
-    discord_thread = threading.Thread(target=start_discord, args=(arguments, ))
-    discord_thread.daemon = True
-    threads.append(discord_thread)
-    website_thread = threading.Thread(target=start_website)
-    threads.append(website_thread)
-    threads[0].start()
-    #threads[1].start()
-    threads[0].join()
-    #threads[1].join()
+    website_thread = None
+    discord_thread = None
+
+    # create threads
+    if arguments.get("--discord_only", False) or arguments.get("--website_only", False):
+        logging.info("Creating Discord Thread")
+        discord_thread = threading.Thread(target=start_discord, args=(arguments,))
+        discord_thread.daemon = True
+        discord_thread.start()
+        discord_thread.join()
+    elif arguments.get("--website_only", False) or arguments.get("--discord_only", False):
+        logging.info("Creating Website Thread")
+        website_thread = threading.Thread(target=start_website)
+        website_thread.start()
+        website_thread.join()
+
+    # start threads
+    logging.info("Starting Threads")
+    if discord_thread:
+        discord_thread.start()
+    if website_thread:
+        website_thread.start()
+
+    # wait for threads to terminate
+    logging.info("Waiting for Threads to Complete")
+    if discord_thread:
+        discord_thread.join()
+    if website_thread:
+        website_thread.join()
